77d927155d2dd4a9c9244f9df92072faeca17bb3
小滴技术面试官 <>
2024-12-19 17:49:09 +0800

    niit

---
+++ src/test/java/net/xdclass/DcloudAipanApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xdclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcloudAipanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
