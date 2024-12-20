5b25e4ad2c6c3451f99c0f8bf858f6f2400866c3
小滴技术面试官 <>
2024-12-20 17:00:56 +0800

    测试

---
+++ src/main/java/net/xdclass/config/MinioConfig.java
@@ -0,0 +1,46 @@
+package net.xdclass.config;
+
+import io.minio.MinioClient;
+import lombok.Data;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * 小滴课堂,愿景：让技术不再难学
+ *
+ * @Description
+ * @Author 二当家小D
+ * @Remark 有问题直接联系我，源码-笔记-技术交流群,官网 https://xdclass.net
+ * @Version 1.0
+ **/
+@Data
+@Component
+@ConfigurationProperties(prefix = "minio")
+public class MinioConfig {
+
+    @Value("endpoint")
+    private String endpoint;
+
+    @Value("access-key")
+    private String accessKey;
+
+    @Value("access-secret")
+    private String accessSecret;
+
+    @Value("bucket-name")
+    private String bucketName;
+
+    //预签名的URL过期时间 ms 毫秒
+    private Long PRE_SING_URL_EXPIRE_TIME = 60 * 10 * 1000L;
+
+
+    @Bean
+    public MinioClient getMinIoClient(){
+        return MinioClient.builder().endpoint(endpoint).credentials(accessKey, accessSecret).build();
+    }
+
+
+}
---
+++ src/main/java/net/xdclass/controller/TestController.java
@@ -0,0 +1,59 @@
+package net.xdclass.controller;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import jakarta.validation.constraints.Min;
+import net.xdclass.config.MinioConfig;
+import net.xdclass.util.CommonUtil;
+import net.xdclass.util.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+/**
+ * 小滴课堂,愿景：让技术不再难学
+ *
+ * @Description
+ * @Author 二当家小D
+ * @Remark 有问题直接联系我，源码-笔记-技术交流群,官网 https://xdclass.net
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/api/test/v1")
+public class TestController {
+
+
+    @Autowired
+    private MinioConfig minioConfig;
+
+    @Autowired
+    private MinioClient minioClient;
+
+    @PostMapping("upload")
+    public JsonData testUpload(@RequestParam("file") MultipartFile file){
+
+        String filename = CommonUtil.getFilePath(file.getOriginalFilename());
+
+        //读取文件流，上传到minio
+        try {
+            InputStream inputStream = file.getInputStream();
+
+            minioClient.putObject(PutObjectArgs.builder().bucket(minioConfig.getBucketName())
+                    .object(filename)
+                    .stream(inputStream, inputStream.available(), -1)
+                    .build());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String url = minioConfig.getEndpoint() + "/" + minioConfig.getBucketName() + "/" + filename;
+        return JsonData.buildSuccess(url);
+    }
+
+}
