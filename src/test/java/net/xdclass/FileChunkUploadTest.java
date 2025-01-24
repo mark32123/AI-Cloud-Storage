d3d9f5e35d3d4c283181f21913f5278120e0a768
小滴技术面试官 <>
2025-01-24 15:29:18 +0800

    save file

--- src/test/java/net/xdclass/FileChunkUploadTest.java
+++ src/test/java/net/xdclass/FileChunkUploadTest.java
@@ -1,7 +1,16 @@
 package net.xdclass;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import net.xdclass.controller.req.FileChunkInitTaskReq;
+import net.xdclass.controller.req.FileChunkMergeReq;
+import net.xdclass.dto.FileChunkDTO;
 import net.xdclass.service.FileChunkService;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +19,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 小滴课堂,愿景：让技术不再难学
@@ -49,7 +60,7 @@ public class FileChunkUploadTest {
     /**
      * 分片大小 5MB
      */
-    private final int chunkSize = 1024 * 1024 * 5;
+    private final long chunkSize = 1024 * 1024 * 5;
 
 
     /**
@@ -66,7 +77,7 @@ public class FileChunkUploadTest {
         int chunkNum = (int) Math.ceil(filesize * 1.0 / chunkSize);
         log.info("分片数量:{}",chunkNum);
         try(FileInputStream fis = new FileInputStream(file)){
-            byte[] buffer = new byte[chunkSize];
+            byte[] buffer = new byte[(int)chunkSize];
             for(int i = 0; i<chunkNum ; i++){
                 String chunkFileName = filepath + ".part"+(i+1);
                 try(FileOutputStream fos = new FileOutputStream(chunkFileName)){
@@ -80,8 +91,84 @@ public class FileChunkUploadTest {
         }catch (Exception e){
             e.printStackTrace();
         }
+
+        //创建分片上传任务
+        testInitFileChunkTask();
+
     }
 
+    /**
+     * 1-创建分片上传任务
+     */
+    private void testInitFileChunkTask() {
+        FileChunkInitTaskReq req = new FileChunkInitTaskReq();
+        req.setAccountId(accountId).setFilename("es_note.pdf")
+                .setIdentifier(identifier)
+                .setTotalSize((long)20552959)
+                .setChunkSize(chunkSize);
+        FileChunkDTO fileChunkDTO = fileChunkService.initFileChunkTask(req);
+        log.info("初始化分片上传任务:{}",fileChunkDTO);
+
+        uploadId = fileChunkDTO.getUploadId();
+
+        //获取分片临时上传地址
+        testGetFileChunkUploadUrl();
+    }
+
+    /**
+     * 2-获取分片临时上传地址
+     */
+    private void testGetFileChunkUploadUrl() {
+        for(int i =1; i<=chunkFilePaths.size(); i++){
+            String uploadUrl = fileChunkService.genPreSignUploadUrl(accountId, identifier, i);
+            log.info("分片上传地址:{}",uploadUrl);
+            //存储分片上传地址
+            chunkUploadUrls.add(uploadUrl);
+        }
+
+        //上传分片文件，模拟前端上传
+        uploadChunk();
+
+    }
+
+    /**
+     * 模拟前端上传分片文件到minio
+     */
+    @SneakyThrows
+    private void uploadChunk() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        for(int i=0; i<chunkUploadUrls.size(); i++){
+            //使用对应put方法直接上传
+            String uploadUrl = chunkUploadUrls.get(i);
+            HttpPut httpPut = new HttpPut(uploadUrl);
+            httpPut.setHeader("Content-Type","application/octet-stream");
+            File chunkFile = new File(chunkFilePaths.get(i));
+            FileEntity chunkFileEntity = new FileEntity(chunkFile);
+            httpPut.setEntity(chunkFileEntity);
+            CloseableHttpResponse response = httpClient.execute(httpPut);
+            log.info("分片上传状态:{}",response.getStatusLine());
+        }
+
+    }
+
+    /**
+     * 大文件合并接口测试
+     */
+    @Test
+    public void testMergeFileChunk(){
+        FileChunkMergeReq req = new FileChunkMergeReq();
+        req.setAccountId(accountId).setIdentifier(identifier).setParentId(1877928108187836417L);
+        fileChunkService.mergeFileChunk(req);
+    }
+
+    /**
+     * 查询文件上传进度测试
+     */
+    @Test
+    public void testChunkUploadProgress(){
+        FileChunkDTO fileChunkDTO = fileChunkService.listFileChunk(accountId, identifier);
+        log.info("查询文件上传进度:{}",fileChunkDTO);
+    }
 
 
 }
