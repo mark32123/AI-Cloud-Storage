3e9258d7e5e0afbd4516dff56f6fedafd70e8fbc
小滴技术面试官 <>
2025-02-20 16:35:07 +0800

    bug

--- src/test/java/net/xdclass/DcloudAipanApplicationTests.java
+++ src/test/java/net/xdclass/DcloudAipanApplicationTests.java
@@ -1,13 +1,30 @@
 package net.xdclass;
 
+import com.alibaba.dashscope.aigc.generation.GenerationOutput;
+import com.alibaba.dashscope.aigc.generation.GenerationResult;
+import lombok.SneakyThrows;
+import net.xdclass.service.ChatService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DcloudAipanApplicationTests {
 
+
+
+	@Autowired
+	private ChatService chatService;
+
 	@Test
-	void contextLoads() {
+	@SneakyThrows
+	public void testAI(){
+		GenerationResult generationResult = chatService.callWithMessage("你是谁，帮我写个Java的冒泡排序");
+		GenerationOutput output = generationResult.getOutput();
+		String content = output.getChoices().get(0).getMessage().getContent();
+		System.out.println(content);
+		System.exit(0);
+
 	}
 
 }
